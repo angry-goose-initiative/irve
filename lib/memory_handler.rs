3310a6dabe917dd68542ef98cb08a8a6c50460f6
John Zacarias Jekel <jzjisawesome123@hostmail.com>
2023-04-11 20:03:58 +0000

    Progress with memory handling

--- lib/memory_handler.rs
+++ lib/memory_handler.rs
@@ -28,13 +28,23 @@ use crate::state::State;
 //pub struct InstructionHandler {
     //TODO
 //}
+//
 
+#[derive(Debug, Clone, Copy)]
+pub enum AccessType {
+    Fetch,
+    Read,
+    Write
+}
+
+//TODO add access size as well
+
+#[derive(Debug, Clone, Copy)]
 pub enum MatchCriteria {
-    Always,
-    SingleAddress(u32),
-    AddressRange(u32, u32),//Inclusive
-
-    Never,
+    Always(AccessType),
+    SingleAddress(AccessType, u32),
+    AddressRange(AccessType, u32, u32),//Inclusive
+    Never
 
     //TODO
 }
@@ -43,9 +53,51 @@ pub enum MatchCriteria {
 //IMPORTANT: Actually, no, the InstructionHandler will be consumed when it is registered
 //and if the user wants to communicate with their own code, they can use a channel
 pub trait MemoryHandler /*<const NUM_CRITERIA: usize>*/ {
-    const MATCH_CRITERIA: MatchCriteria;//[MatchCriteria; NUM_CRITERIA];
+    //const MATCH_CRITERIA: MatchCriteria;//[MatchCriteria; NUM_CRITERIA];
    
-    fn handle(&self, state: &mut State/*, addr: address*/)/* -> Result<(), Error>*/;
+    fn get_match_criteria(&self) -> &[MatchCriteria];
+
+    //Memory handlers are allowed to panic if they are unable to handle the request
+    //So the match criteria should be checked before calling the handler functions
+    fn fetch_byte(&self, state: &mut State, addr: u32) -> u8 {
+        self.read_byte(state, addr)//By default just implement fetch with read
+    }
+    fn read_byte(&self, state: &mut State, addr: u32) -> u8;
+    fn write_byte(&self, state: &mut State, addr: u32, data: u8);
+
+    //The functions that read multiple bytes MUST BE ALIGNED or else they may panic
+    fn fetch_halfword(&self, state: &mut State, addr: u32) -> u16 {
+        self.read_halfword(state, addr)//By default just implement fetch with read
+    }
+    fn read_halfword(&self, state: &mut State, addr: u32) -> u16 {
+        //By default just implement read_halfword with read_byte (LITTLE ENDIAN)
+        let mut data = self.read_byte(state, addr) as u16;
+        data |= (self.read_byte(state, addr + 1) as u16) << 8;
+        data
+    }
+    fn write_halfword(&self, state: &mut State, addr: u32, data: u16) {
+        //By default just implement write_halfword with write_byte (LITTLE ENDIAN)
+        self.write_byte(state, addr, data as u8);
+        self.write_byte(state, addr + 1, (data >> 8) as u8);
+    }
+    fn fetch_word(&self, state: &mut State, addr: u32) -> u32 {
+        self.read_word(state, addr)//By default just implement fetch with read
+    }
+    fn read_word(&self, state: &mut State, addr: u32) -> u32 {
+        //By default just implement read_word with read_byte (LITTLE ENDIAN)
+        let mut data = self.read_byte(state, addr) as u32;
+        data |= (self.read_byte(state, addr + 1) as u32) << 8;
+        data |= (self.read_byte(state, addr + 2) as u32) << 16;
+        data |= (self.read_byte(state, addr + 3) as u32) << 24;
+        data
+    }
+    fn write_word(&self, state: &mut State, addr: u32, data: u32) {
+        //By default just implement write_word with write_byte (LITTLE ENDIAN)
+        self.write_byte(state, addr, data as u8);
+        self.write_byte(state, addr + 1, (data >> 8) as u8);
+        self.write_byte(state, addr + 2, (data >> 16) as u8);
+        self.write_byte(state, addr + 3, (data >> 24) as u8);
+    }
     
     //TODO
 }
@@ -53,12 +105,12 @@ pub trait MemoryHandler /*<const NUM_CRITERIA: usize>*/ {
 /* Associated Functions and Methods */
 
 impl MatchCriteria {
-    pub fn matches(&self, addr: u32) -> bool {
+    pub fn matches(&self, addr: u32, access_type: AccessType) -> bool {
         match self {
-            MatchCriteria::Always               => true,
-            MatchCriteria::SingleAddress(a)     => addr == *a,
-            MatchCriteria::AddressRange(a, b)   => (addr >= *a) && (addr <= *b),
-            MatchCriteria::Never                => false,
+            MatchCriteria::Always(access_type)              => true,
+            MatchCriteria::SingleAddress(access_type, a)    => addr == *a,
+            MatchCriteria::AddressRange(access_type, a, b)  => (addr >= *a) && (addr <= *b),
+            default                                         => false
         }
     }
 }
--- lib/pmmap.rs
+++ lib/pmmap.rs
@@ -9,7 +9,9 @@
 
 /* Imports */
 
-//TODO (include "use" and "mod" here)
+use crate::state::State;
+use crate::memory_handler::MemoryHandler;
+use crate::memory_handler::AccessType;
 
 /* Constants */
 
@@ -25,11 +27,58 @@
 
 /* Types */
 
-//TODO
+struct PhysicalMemoryMap {
+    handlers: Vec<Box<dyn MemoryHandler>>,
+}
 
 /* Associated Functions and Methods */
 
-//TODO
+impl PhysicalMemoryMap {
+    pub fn new() -> Self {
+        Self {
+            handlers: Vec::new(),
+        }
+    }
+
+    //NOTE: 'static in the context of impl DOSN'T mean the handler will live forever
+    //It just means it has no non-static references that could be destroyed while we still possess
+    //it. It could still have references to static things (like strings) or can hold a Sender
+    //so that it can communicate with the user code that created it
+    //This stops the user from ex. passing a reference to a struct where the reference is impl MemoryHandler
+    pub fn register_handler(&mut self, handler: impl MemoryHandler + 'static) {
+        self.handlers.push(Box::new(handler));
+    }
+
+    //TODO functions for reading, writing, and fetching (which will lookup the proper handler)
+    pub fn fetch_byte(&self, state: &mut State, addr: u32) -> Result<u8, ()> {
+        Ok(self.search(addr, AccessType::Fetch)?.fetch_byte(state, addr))
+    }
+    pub fn read_byte(&self, state: &mut State, addr: u32) -> Result<u8, ()> {
+        Ok(self.search(addr, AccessType::Read)?.read_byte(state, addr))
+    }
+    pub fn write_byte(&self, state: &mut State, addr: u32, value: u8) -> Result<(), ()> {
+        self.search(addr, AccessType::Write)?.write_byte(state, addr, value);
+        Ok(())
+    }
+    //TODO what if we can only legally access the first byte in a multi-byte access?
+    //How do we tell if we're allowed to access the rest of the bytes?
+
+    fn search(&self, addr: u32, access_type: AccessType) -> Result<&Box<dyn MemoryHandler>, ()> {
+        //TODO do this in a more efficient way
+        
+        //Linearly search the handlers for a match
+        for handler in &self.handlers {
+            //Linearly search the criteria in each handler for a match
+            for criterion in handler.get_match_criteria() {
+                if criterion.matches(addr, access_type) {
+                    return Ok(handler);
+                }
+            }
+        }
+
+        Err(())
+    }
+}
 
 /* Functions */
 
