7c79e9e685df7dea633aa9f1ee4a2b70870cab50
John Jekel <JZJisawesome123@gmail.com>
2023-04-28 13:49:35 -0400

    Fix bug with register fields in decode due to this originally being for RV32E

--- src/decode.cpp
+++ src/decode.cpp
@@ -33,10 +33,10 @@ decoded_inst_t::decoded_inst_t(uint32_t instruction) {
         case OP:
             this->m_format = R_TYPE;
             this->m_funct7 = (instruction >> 25) & 0b1111111;
-            this->m_rs2 = (instruction >> 20) & 0b1111;
-            this->m_rs1 = (instruction >> 15) & 0b1111;
+            this->m_rs2 = (instruction >> 20) & 0b11111;
+            this->m_rs1 = (instruction >> 15) & 0b11111;
             this->m_funct3 = (instruction >> 12) & 0b111;
-            this->m_rd = (instruction >> 7) & 0b1111;
+            this->m_rd = (instruction >> 7) & 0b11111;
             break;
         //I-type
         case LOAD:
@@ -45,25 +45,25 @@ decoded_inst_t::decoded_inst_t(uint32_t instruction) {
         case SYSTEM:
             this->m_format = I_TYPE;
             this->m_imm = SIGN_EXTEND_TO_32(instruction >> 20, 12);
-            this->m_rs1 = (instruction >> 15) & 0b1111;
+            this->m_rs1 = (instruction >> 15) & 0b11111;
             this->m_funct3 = (instruction >> 12) & 0b111;
-            this->m_rd = (instruction >> 7) & 0b1111;
+            this->m_rd = (instruction >> 7) & 0b11111;
             this->m_funct7 = instruction >> 25;
             break;
         //S-type
         case STORE:
             this->m_format = S_TYPE;
             this->m_imm = SIGN_EXTEND_TO_32(((instruction >> 20) & 0b111111100000) | ((instruction >> 7) & 0b11111), 12);
-            this->m_rs2 = (instruction >> 20) & 0b1111;
-            this->m_rs1 = (instruction >> 15) & 0b1111;
+            this->m_rs2 = (instruction >> 20) & 0b11111;
+            this->m_rs1 = (instruction >> 15) & 0b11111;
             this->m_funct3 = (instruction >> 12) & 0b111;
             break;
         //B-type
         case BRANCH:
             this->m_format = B_TYPE;
             this->m_imm = SIGN_EXTEND_TO_32(((instruction >> 19) & 0b1000000000000) | ((instruction << 4) & 0b100000000000) | ((instruction >> 20) & 0b11111100000) | ((instruction >> 7) & 0b11110), 13);
-            this->m_rs2 = (instruction >> 20) & 0b1111;
-            this->m_rs1 = (instruction >> 15) & 0b1111;
+            this->m_rs2 = (instruction >> 20) & 0b11111;
+            this->m_rs1 = (instruction >> 15) & 0b11111;
             this->m_funct3 = (instruction >> 12) & 0b111;
             break;
         //U-type
@@ -71,13 +71,13 @@ decoded_inst_t::decoded_inst_t(uint32_t instruction) {
         case AUIPC:
             this->m_format = U_TYPE;
             this->m_imm = instruction & 0b11111111111111111111000000000000;
-            this->m_rd = (instruction >> 7) & 0b1111;
+            this->m_rd = (instruction >> 7) & 0b11111;
             break;
         //J-type
         case JAL:
             this->m_format = J_TYPE;
             this->m_imm = SIGN_EXTEND_TO_32(((instruction >> 11) & 0b100000000000000000000) | (instruction & 0b11111111000000000000) | ((instruction >> 9) & 0b100000000000) | ((instruction >> 20) & 0b11111111110), 21);
-            this->m_rd = (instruction >> 7) & 0b1111;
+            this->m_rd = (instruction >> 7) & 0b11111;
             break;
         default:
             this->m_format = INVALID;
