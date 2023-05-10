55732ea3667315180aa246a93d4adc8cf491b276
John Jekel <JZJisawesome123@gmail.com>
2023-05-10 08:07:59 -0400

    Add seperate files to deal with loading which will absorb some code from main.cpp

---
+++ src/loader.cpp
@@ -0,0 +1,35 @@
+/* loader.cpp
+ * Copyright (C) 2023 John Jekel and Nick Chan
+ * See the LICENSE file at the root of the project for licensing info.
+ *
+ * Code for loading RISC-V binaries into memory
+ *
+*/
+
+/* Constants And Defines */
+
+//TODO
+
+/* Includes */
+
+//TODO
+
+/* Types */
+
+//TODO
+
+/* Variables */
+
+//TODO
+
+/* Static Function Declarations */
+
+//TODO
+
+/* Function Implementations */
+
+//TODO
+
+/* Static Function Implementations */
+
+//TODO
---
+++ src/loader.h
@@ -0,0 +1,32 @@
+/* loader.cpp
+ * Copyright (C) 2023 John Jekel and Nick Chan
+ * See the LICENSE file at the root of the project for licensing info.
+ *
+ * Code for loading RISC-V binaries into memory
+ *
+*/
+
+#ifndef LOADER_H
+#define LOADER_H
+
+/* Includes */
+
+//TODO
+
+/* Constants And Defines */
+
+//TODO
+
+/* Types */
+
+//TODO
+
+/* Global Variables */
+
+//TODO
+
+/* Function/Class Declarations */
+
+//TODO
+
+#endif//LOADER_H
