dd3050be3121d53a791f3ae93c879da338f40336
John Zacarias Jekel <JZJisawesome123@hotmail.com>
2023-05-12 10:21:46 +0000

    Disable gcc sanitization options by default

--- tests/integration/basics.cpp
+++ tests/integration/basics.cpp
@@ -15,13 +15,20 @@
 
 /* Function Implementations */
 
-int test_basics() {//None of these functions should crash or throw exceptions
+int test_about() {
+    //None of these functions should crash or throw exceptions
     std::cerr << "libirve " << irve::about::get_version_string() << std::endl;
     std::cerr << "libirve built at " << irve::about::get_build_time_string() << " on " << irve::about::get_build_date_string() << std::endl;
     std::cerr << "Build system: " << irve::about::get_build_system_string() << std::endl;
     std::cerr << "Build from " << irve::about::get_build_host_string() << " for " << irve::about::get_compile_target_string() << std::endl;
     std::cerr << "Compiler: " << irve::about::get_compiler_string() << std::endl;
-    std::cerr << "Logging is " << (irve::logging::logging_disabled() ? "disabled" : "enabled") << std::endl;
+    
+    return 0;
+}
+
+int test_logging() {
+    //None of these functions should crash or throw exceptions
+    irve::logging::log(0, "Logging is %s", irve::logging::logging_disabled() ? "disabled" : "enabled"); 
 
     for (int i = -1; i < 123; i++) {
         irve::logging::log(i, "This is a test log message");
@@ -29,8 +36,9 @@ int test_basics() {//None of these functions should crash or throw exceptions
         irve::logging::log(i - 1, "Negative indent hopefully won't crash %f", 3.14159);
         irve::logging::log(i, "This is a message with a string: %s", "Hello, world!");
         irve::logging::log(i, "This is a message with a char: %c", 'A');
-        irve::logging::log(i, "This is a message with a pointer: %p", test_basics);
+        irve::logging::log(i, "This is a message with a pointer: %p", test_logging);
+        //TODO stress test it more?
     }
-    
+
     return 0;
 }
