4e1013c8c4035a6559c11d257de512d272ae12ed
John Jekel <JZJisawesome123@gmail.com>
2023-05-15 22:14:13 -0400

    Add the option to do async logging (10x emulation speed increase when logging is enabled

--- lib/logging.cpp
+++ lib/logging.cpp
@@ -9,36 +9,144 @@
 
 /* Includes */
 
+#include "config.h"
+
 #define INST_COUNT this does not actually need to be defined with anything important before including logging.h in this case
 #include "logging.h"
 
 #include <cassert>
 #include <cstdarg>
 #include <cstdio>
+#include <cstdint>
+#include <cstdlib>
+
+#if IRVE_INTERNAL_CONFIG_ASYNC_LOGGING
+#include <string>
+#include <thread>
+#include <atomic>
+#include <queue>
+#include <tuple>
+#endif
 
 using namespace irve::internal;
 
-/* Function Implementations */
+/* Static Functions */
 
-void logging::irvelog_internal_function_dont_use_this_directly(FILE* destination, uint64_t inst_num, uint8_t indent, const char* str, ...) {
-    va_list list;
-    va_start(list, str);
-    irvelog_internal_function_dont_use_this_directly(destination, inst_num, indent, str, list);
-}
-
-void logging::irvelog_internal_function_dont_use_this_directly(FILE* destination, uint64_t inst_num, uint8_t indent, const char* str, va_list list) {
-    assert(str && "irvelog called with null format string");
+static void actual_log_function(FILE* destination, uint64_t inst_num, uint8_t indent, const char* str) {
+    assert(destination && "Attempt to log to null destination file");
+    assert(str && "Attempt to log with null string");
 
     if (inst_num) {
-        fprintf(destination, "\x1b[94m%llu\x1b[1;90m>\x1b[0m ", (long long unsigned int) inst_num);
+        std::fprintf(destination, "\x1b[94m%llu\x1b[1;90m>\x1b[0m ", (long long unsigned int) inst_num);
     } else {
-        fprintf(destination, "\x1b[94mIRVE\x1b[1;90m>\x1b[0m ");
+        std::fprintf(destination, "\x1b[94mIRVE\x1b[1;90m>\x1b[0m ");
     }
 
     while (indent--) {
-        fprintf(destination, "  ");
+        std::fprintf(destination, "  ");
     }
 
-    vfprintf(destination, str, list);
-    putc('\n', destination);
+    std::fprintf(destination, "%s\n", str);
 }
+
+/* Function Implementations */
+
+void logging::irvelog_internal_variadic_function_dont_use_this_directly(FILE* destination, uint64_t inst_num, uint8_t indent, const char* str, ...) {
+    va_list list_copy_1;
+    va_start(list_copy_1, str);
+    va_list list_copy_2;
+    va_copy(list_copy_2, list_copy_1);
+
+    //Perform variable argument string formatting into a buffer
+    std::size_t buffer_size = std::vsnprintf(nullptr, 0, str, list_copy_1) + 1;//vsnprintf doesn't count the null terminator
+    va_end(list_copy_1);
+    char* buffer = (char*)std::malloc(buffer_size);
+    std::vsnprintf(buffer, buffer_size, str, list_copy_2);
+    va_end(list_copy_2);
+
+    //Actually log the string
+    irvelog_internal_function_dont_use_this_directly(destination, inst_num, indent, buffer);
+
+    //Free the buffer
+    std::free(buffer);
+}
+
+#if IRVE_INTERNAL_CONFIG_ASYNC_LOGGING
+
+/* Internal Classes */
+
+class AsyncLogger {
+    private:
+        std::queue<std::tuple<FILE*, uint64_t, uint8_t, std::string>> m_queue;
+        std::atomic<bool> m_queue_busy;
+        std::atomic<bool> m_thread_running;
+        std::thread m_thread;
+    public:
+        AsyncLogger() : m_queue_busy(false), m_thread_running(true), m_thread([&]() {
+            puts("Hello from thread!");
+
+            //Main logging loop
+            while (true) {
+                //Continually try to reserve the queue
+                while (m_queue_busy.exchange(true)) {
+                    std::this_thread::yield();
+                }
+                //At this point, the queue is reserved
+
+                //If the queue is empty, release it and check if the thread should exit
+                if (m_queue.empty()) {
+                    m_queue_busy.store(false);
+
+                    if (!m_thread_running.load()) {//The queue is empty and will never be filled again
+                        return;//So exit the thread
+                    }
+                } else {//Otherwise, if the queue is not empty
+                    //Pop the front element and release the queue
+                    auto [destination, inst_num, indent, str] = m_queue.front();
+                    m_queue.pop();
+                    m_queue_busy.store(false);
+
+                    //Log the popped element
+                    actual_log_function(destination, inst_num, indent, str.c_str());
+                }
+            }
+        })
+        {}
+
+        ~AsyncLogger() {
+            m_thread_running.store(false);
+            m_thread.join();
+        }
+
+        void enqueue_log_request(FILE* destination, uint64_t inst_num, uint8_t indent, std::string str) {
+            while (m_queue_busy.exchange(true));//Busy wait until the queue is free
+            m_queue.emplace(destination, inst_num, indent, str);
+            m_queue_busy.store(false);//Release the queue
+        }
+};
+
+/* Variables */
+
+static AsyncLogger async_logger;
+
+/* Function Implementations */
+
+void logging::irvelog_internal_function_dont_use_this_directly(FILE* destination, uint64_t inst_num, uint8_t indent, const char* str) {
+    assert(destination && "Attempt to log to null destination file");
+    assert(str && "Attempt to log with null string");
+    
+    async_logger.enqueue_log_request(destination, inst_num, indent, str);
+}
+
+#else
+
+/* Function Implementations */
+
+void logging::irvelog_internal_function_dont_use_this_directly(FILE* destination, uint64_t inst_num, uint8_t indent, const char* str) {
+    assert(destination && "Attempt to log to null destination file");
+    assert(str && "Attempt to log with null string");
+    
+    actual_log_function(destination, inst_num, indent, str);
+}
+
+#endif
--- lib/logging.h
+++ lib/logging.h
@@ -19,6 +19,9 @@
 
 /* Constants And Defines */
 
+//The raw versions of the logging functions are faster, but don't have any formatting
+//TODO make the raw version the default, and varadic opt-in with virvelog
+
 #ifndef INST_COUNT
 #error "INST_COUNT must be defined before including logging.h"
 #endif
@@ -31,30 +34,57 @@
     ((void)(indent)); \
 } while (0)
 
+#define irvelog_raw(indent, str) do { \
+    ((void)(INST_COUNT)); \
+    ((void)(indent)); \
+    ((void)(str)); \
+} while (0)
+
 #else
 
 #define irvelog(indent, ...) do { \
     irvelog_always(indent, __VA_ARGS__); \
 } while (0)
 
+#define irvelog_raw(indent, str) do { \
+    irvelog_raw_always(indent, str); \
+} while (0)
+
 #endif
 
 //USE THIS SPARINGLY
+#define irvelog_raw_always(indent, str) do { \
+    irve::internal::logging::irvelog_internal_function_dont_use_this_directly(stderr, INST_COUNT, indent, str); \
+} while (0)
+
+//USE THIS SPARINGLY
 #define irvelog_always(indent, ...) do { \
-    irve::internal::logging::irvelog_internal_function_dont_use_this_directly(stderr, INST_COUNT, indent, __VA_ARGS__); \
+    irve::internal::logging::irvelog_internal_variadic_function_dont_use_this_directly(stderr, INST_COUNT, indent, __VA_ARGS__); \
+} while (0)
+
+//SHOULD ONLY BE USED FOR RISC-V OUTPUT
+#define irvelog_raw_always_stdout(indent, str) do { \
+    irve::internal::logging::irvelog_internal_function_dont_use_this_directly(stdout, INST_COUNT, indent, str); \
 } while (0)
 
 //SHOULD ONLY BE USED FOR RISC-V OUTPUT
 #define irvelog_always_stdout(indent, ...) do { \
-    irve::internal::logging::irvelog_internal_function_dont_use_this_directly(stdout, INST_COUNT, indent, __VA_ARGS__); \
+    irve::internal::logging::irvelog_internal_variadic_function_dont_use_this_directly(stdout, INST_COUNT, indent, __VA_ARGS__); \
 } while (0)
 
 /* Function/Class Declarations */
 
 namespace irve::internal::logging {
+//#if IRVE_INTERNAL_CONFIG_ASYNC_LOGGING
+    //void init();
+    //void irvelog_async_deinit();
+    //void irvelog_async_flush();
+//#endif
+
     //TODO have -1 mean end
-    void irvelog_internal_function_dont_use_this_directly(FILE* destination, uint64_t inst_num, uint8_t indent, const char* str, ...);
-    void irvelog_internal_function_dont_use_this_directly(FILE* destination, uint64_t inst_num, uint8_t indent, const char* str, va_list list);
+    void irvelog_internal_variadic_function_dont_use_this_directly(FILE* destination, uint64_t inst_num, uint8_t indent, const char* str, ...);
+    void irvelog_internal_function_dont_use_this_directly(FILE* destination, uint64_t inst_num, uint8_t indent, const char* str);
+    //void irvelog_internal_function_dont_use_this_directly(FILE* destination, uint64_t inst_num, uint8_t indent, const char* str, va_list list);
 }
 
 #endif//LOGGING_H
