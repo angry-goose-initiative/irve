b3e4e35b99fcd22c3550af253e086305dd3c09c9
John Jekel <JZJisawesome123@gmail.com>
2023-05-21 12:32:01 -0400

    Uncover issues with the lh and lb implementations

--- tests/testfiles/jzjcoresoftware.cpp
+++ tests/testfiles/jzjcoresoftware.cpp
@@ -26,6 +26,10 @@
     irve::loader::load_verilog_32(emulator, "jzjcoresoftware/assembly/" program_name ".hex"); \
     irve::internal::cpu_state::cpu_state_t& cpu_state_ref = emulator.m_emulator_ptr->m_cpu_state;
 
+/* Static Function Declarations */
+
+static int verify_jzjcoresoftware_fibonacci(irve::emulator::emulator_t& emulator, irve::internal::cpu_state::cpu_state_t& cpu_state_ref);
+
 /* Function Implementations */
 
 //TODO also check CSRs, memory, etc
@@ -176,53 +180,148 @@ int verify_jzjcoresoftware_fenceecalltest() {
 int verify_jzjcoresoftware_fibbonaccijal() {
     //Load the fibbonaccijal program
     setup_emulator_with_program("fibbonaccijal");
-    uint64_t expected_inst_count = 0;
-    uint32_t expected_x29 = 0;
-    uint32_t expected_x30 = 1;
-    uint32_t expected_x31 = 0;
 
-    //Checks things are as expected at the start
-    emulator.tick();//Execute the first addi
-    assert((emulator.get_inst_count() == ++expected_inst_count));
-    assert((cpu_state_ref.get_pc() == 0x4));
-    assert((cpu_state_ref.get_r(29) == expected_x29));
-    emulator.tick();//Execute the second addi
-    assert((emulator.get_inst_count() == ++expected_inst_count));
-    assert((cpu_state_ref.get_pc() == 0x8));
-    assert((cpu_state_ref.get_r(30) == expected_x30));
-    emulator.tick();//Execute the third addi
-    assert((emulator.get_inst_count() == ++expected_inst_count));
-    assert((cpu_state_ref.get_pc() == 0xC));
-    assert((cpu_state_ref.get_r(31) == expected_x31));
-    
-    //In the loop
-    for (uint32_t i = 0; i < 47; ++i) {//Max fibonacci number that can fit in a 32 bit register is the 47th
-        emulator.tick();//Execute the add
-        expected_x31 = expected_x29 + expected_x30;
-        assert((emulator.get_inst_count() == ++expected_inst_count));
-        assert((cpu_state_ref.get_pc() == 0x10));
-        assert((cpu_state_ref.get_r(31) == expected_x31));
-        emulator.tick();//Execute the first addi
-        expected_x29 = expected_x30;
-        assert((emulator.get_inst_count() == ++expected_inst_count));
-        assert((cpu_state_ref.get_pc() == 0x14));
-        assert((cpu_state_ref.get_r(29) == expected_x29));
-        emulator.tick();//Execute the second addi
-        expected_x30 = expected_x31;
-        assert((emulator.get_inst_count() == ++expected_inst_count));
-        assert((cpu_state_ref.get_pc() == 0x18));
-        assert((cpu_state_ref.get_r(30) == expected_x30));
-        emulator.tick();//Execute the jal
-        assert((emulator.get_inst_count() == ++expected_inst_count));
-        assert((cpu_state_ref.get_pc() == 0xC));
-    }
-
-    return 0;
+    return verify_jzjcoresoftware_fibonacci(emulator, cpu_state_ref);
 }
 
 int verify_jzjcoresoftware_fibbonaccijalr() {
     //Load the fibbonaccijal program
     setup_emulator_with_program("fibbonaccijalr");
+
+    return verify_jzjcoresoftware_fibonacci(emulator, cpu_state_ref);
+}
+
+int verify_jzjcoresoftware_lbutest() {
+    //Load the lbutest program
+    setup_emulator_with_program("lbutest");
+    uint64_t expected_inst_count = 0;
+
+    //This testcase assumes x30 starts at 0 (it forgot to set it)
+    cpu_state_ref.set_r(30, 0);
+
+    //Check the loop
+    const uint32_t expected_x31[16] = {
+        0x01, 0x23, 0x45, 0x67,
+        0x89, 0xAB, 0xCD, 0xEF,
+        0xFE, 0xDC, 0xBA, 0x98,
+        0x76, 0x54, 0x32, 0x10
+    };
+    for (uint32_t i = 0; i < 16; ++i) {
+        emulator.tick();//Execute the lbu
+        assert((emulator.get_inst_count() == ++expected_inst_count));
+        assert((cpu_state_ref.get_pc() == 0x4));
+        assert((cpu_state_ref.get_r(31) == expected_x31[i]));
+        emulator.tick();//Execute the addi
+        assert((emulator.get_inst_count() == ++expected_inst_count));
+        assert((cpu_state_ref.get_pc() == 0x8));
+        assert((cpu_state_ref.get_r(30) == (i + 1)));
+        emulator.tick();//Execute the jalr
+        assert((emulator.get_inst_count() == ++expected_inst_count));
+        assert((cpu_state_ref.get_pc() == 0x0));
+    }
+
+    return 0;
+}
+
+int verify_jzjcoresoftware_lhtest() {
+    //Load the lhtest program
+    setup_emulator_with_program("lhtest");
+    uint64_t expected_inst_count = 0;
+
+    //This testcase assumes x30 starts at 0 (it forgot to set it)
+    cpu_state_ref.set_r(30, 0);
+
+    //Check the loop
+    const uint32_t expected_x31[8] = {
+        0x2301, 0x6745,
+        0xFFFFAB89, 0xFFFFEFCD,//Sign extension
+        0xFFFFDCFE, 0xFFFF98BA,//Sign extension
+        0x5476, 0x1032
+    };
+    for (uint32_t i = 0; i < 8; ++i) {
+        emulator.tick();//Execute the lh
+        assert((emulator.get_inst_count() == ++expected_inst_count));
+        assert((cpu_state_ref.get_pc() == 0x4));
+        assert((cpu_state_ref.get_r(31) == expected_x31[i]));
+        emulator.tick();//Execute the addi
+        assert((emulator.get_inst_count() == ++expected_inst_count));
+        assert((cpu_state_ref.get_pc() == 0x8));
+        assert((cpu_state_ref.get_r(30) == ((i + 1) * 2)));
+        emulator.tick();//Execute the jalr
+        assert((emulator.get_inst_count() == ++expected_inst_count));
+        assert((cpu_state_ref.get_pc() == 0x0));
+    }
+
+    return 0;
+}
+
+int verify_jzjcoresoftware_lhutest() {
+    //Load the lhutest program
+    setup_emulator_with_program("lhutest");
+    uint64_t expected_inst_count = 0;
+
+    //This testcase assumes x30 starts at 0 (it forgot to set it)
+    cpu_state_ref.set_r(30, 0);
+
+    //Check the loop
+    const uint32_t expected_x31[8] = {
+        0x2301, 0x6745,
+        0xAB89, 0xEFCD,
+        0xDCFE, 0x98BA,
+        0x5476, 0x1032
+    };
+    for (uint32_t i = 0; i < 8; ++i) {
+        emulator.tick();//Execute the lhu
+        assert((emulator.get_inst_count() == ++expected_inst_count));
+        assert((cpu_state_ref.get_pc() == 0x4));
+        assert((cpu_state_ref.get_r(31) == expected_x31[i]));
+        emulator.tick();//Execute the addi
+        assert((emulator.get_inst_count() == ++expected_inst_count));
+        assert((cpu_state_ref.get_pc() == 0x8));
+        assert((cpu_state_ref.get_r(30) == ((i + 1) * 2)));
+        emulator.tick();//Execute the jalr
+        assert((emulator.get_inst_count() == ++expected_inst_count));
+        assert((cpu_state_ref.get_pc() == 0x0));
+    }
+
+    return 0;
+}
+
+int verify_jzjcoresoftware_loadbytetest() {
+    //Load the loadbytetest program
+    setup_emulator_with_program("loadbytetest");
+    uint64_t expected_inst_count = 0;
+
+    //This testcase assumes x30 starts at 0 (it forgot to set it)
+    cpu_state_ref.set_r(30, 0);
+
+    //Check the loop
+    const uint32_t expected_x31[16] = {
+        0x01, 0x23, 0x45, 0x67,
+        0xFFFFFF89, 0xFFFFFFAB, 0xFFFFFFCD, 0xFFFFFFEF,
+        0xFFFFFFFE, 0xFFFFFFDC, 0xFFFFFFBA, 0xFFFFFF98,
+        0x76, 0x54, 0x32, 0x10
+    };
+    for (uint32_t i = 0; i < 16; ++i) {
+        emulator.tick();//Execute the lb
+        assert((emulator.get_inst_count() == ++expected_inst_count));
+        assert((cpu_state_ref.get_pc() == 0x4));
+        assert((cpu_state_ref.get_r(31) == expected_x31[i]));
+        emulator.tick();//Execute the addi
+        assert((emulator.get_inst_count() == ++expected_inst_count));
+        assert((cpu_state_ref.get_pc() == 0x8));
+        assert((cpu_state_ref.get_r(30) == (i + 1)));
+        emulator.tick();//Execute the jalr
+        assert((emulator.get_inst_count() == ++expected_inst_count));
+        assert((cpu_state_ref.get_pc() == 0x0));
+    }
+
+    return 0;
+}
+
+/* Static Function Implementations */
+
+static int verify_jzjcoresoftware_fibonacci(irve::emulator::emulator_t& emulator, irve::internal::cpu_state::cpu_state_t& cpu_state_ref) {
     uint64_t expected_inst_count = 0;
     uint32_t expected_x29 = 0;
     uint32_t expected_x30 = 1;
@@ -259,7 +358,7 @@ int verify_jzjcoresoftware_fibbonaccijalr() {
         assert((emulator.get_inst_count() == ++expected_inst_count));
         assert((cpu_state_ref.get_pc() == 0x18));
         assert((cpu_state_ref.get_r(30) == expected_x30));
-        emulator.tick();//Execute the jalr
+        emulator.tick();//Execute the jal or jalr
         assert((emulator.get_inst_count() == ++expected_inst_count));
         assert((cpu_state_ref.get_pc() == 0xC));
     }
