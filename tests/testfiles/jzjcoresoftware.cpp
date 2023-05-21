50da817267a593c04acd5bd984d74f34d399216f
John Jekel <JZJisawesome123@gmail.com>
2023-05-21 10:22:33 -0400

    Test bneandsubtest

--- tests/testfiles/jzjcoresoftware.cpp
+++ tests/testfiles/jzjcoresoftware.cpp
@@ -26,6 +26,8 @@
 
 /* Function Implementations */
 
+//TODO also check CSRs, memory, etc
+
 int verify_jzjcoresoftware_adding2() {
     //Load adding2 program
     setup_emulator_with_program("adding2");
@@ -51,13 +53,66 @@ int verify_jzjcoresoftware_adding2() {
 int verify_jzjcoresoftware_auipctest() {
     //Load the auipctest program
     setup_emulator_with_program("auipctest");
+    irve::internal::cpu_state::cpu_state_t& cpu_state_ref = emulator.m_emulator_ptr->m_cpu_state;
     emulator.run_until(0);
     
     //Checks things are as expected
     assert((emulator.get_inst_count() == 5));//3 nops, 1 auipc, 1 IRVE.EXIT
-    irve::internal::cpu_state::cpu_state_t& cpu_state_ref = emulator.m_emulator_ptr->m_cpu_state;
     assert((cpu_state_ref.get_pc() == 0x10));//5 4-byte instructions
     assert((cpu_state_ref.get_r(31) == ((1 << 12) + 0xC)));//Expected result of auipc
 
+    return 0;
+}
+
+int verify_jzjcoresoftware_bneandsubtest() {
+    //Load the auipctest program
+    setup_emulator_with_program("bneandsubtest");
+    irve::internal::cpu_state::cpu_state_t& cpu_state_ref = emulator.m_emulator_ptr->m_cpu_state;
+    uint64_t expected_inst_count = 0;
+    
+    //Checks things are as expected
+    emulator.tick();
+    assert((emulator.get_inst_count() == ++expected_inst_count));
+    assert((cpu_state_ref.get_pc() == 0x4));
+    assert((cpu_state_ref.get_r(31) == 1000));
+    emulator.tick();
+    assert((emulator.get_inst_count() == ++expected_inst_count));
+    assert((cpu_state_ref.get_pc() == 0x8));
+    assert((cpu_state_ref.get_r(31) == 1000));
+    assert((cpu_state_ref.get_r(30) == 500));
+    emulator.tick();
+    assert((emulator.get_inst_count() == ++expected_inst_count));
+    assert((cpu_state_ref.get_pc() == 0xC));
+    assert((cpu_state_ref.get_r(31) == 1000));
+    assert((cpu_state_ref.get_r(30) == 500));
+    assert((cpu_state_ref.get_r(29) == 5));
+
+    //Now we get to the loop
+    uint32_t expected_r31 = 1000;
+    while (expected_r31 >= 510) {
+        emulator.tick();//Execute the sub
+        expected_r31 -= 5;
+        assert((emulator.get_inst_count() == ++expected_inst_count));
+        assert((cpu_state_ref.get_pc() == 0x10));
+        assert((cpu_state_ref.get_r(31) == expected_r31));
+        emulator.tick();//Execute the bne
+        assert((emulator.get_inst_count() == ++expected_inst_count));
+        assert((cpu_state_ref.get_pc() == 0xC));
+    }
+
+    //On the last iteration, the bne should not be taken
+    emulator.tick();//Execute the sub
+    expected_r31 -= 5;
+    assert((emulator.get_inst_count() == ++expected_inst_count));
+    assert((cpu_state_ref.get_pc() == 0x10));
+    assert((cpu_state_ref.get_r(31) == expected_r31));
+    emulator.tick();//Execute the bne
+    assert((emulator.get_inst_count() == ++expected_inst_count));
+    assert((cpu_state_ref.get_pc() == 0x14));
+
+    emulator.run_until(0);//Execute the IRVE.EXIT
+    assert((emulator.get_inst_count() == ++expected_inst_count));
+    assert((cpu_state_ref.get_pc() == 0x14));
+    
     return 0;
 }
