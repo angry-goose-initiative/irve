05a62cf63c08fbc081ea34e73572ff7d14441518
John Jekel <JZJisawesome123@gmail.com>
2023-05-22 12:53:45 -0400

    Rust documentation work

--- lib/disassemble/lib/ffi.rs
+++ lib/disassemble/lib/ffi.rs
@@ -21,11 +21,22 @@
 #[repr(C)]
 pub enum Format {
     //NOTE: It is imperative these numbers match those in irve::internal::decode::inst_format_t over on the C++ side of things
+    ///R-type RISC-V Instruction
     R = 0,
+    
+    ///I-type RISC-V Instruction
     I = 1,
+    
+    ///S-type RISC-V Instruction
     S = 2,
+    
+    ///B-type RISC-V Instruction
     B = 3,
+    
+    ///U-type RISC-V Instruction
     U = 4,
+    
+    ///J-type RISC-V Instruction
     J = 5
 }
 
@@ -43,6 +54,7 @@ pub enum Format {
 pub enum Opcode {
     //NOTE: It is imperative these numbers match those in irve::internal::decode::opcode_t over on the C++ side of things
     //TODO better formatting of this
+    //TODO documentations of these
     Load = 0b00000,     LoadFp = 0b00001,  Custom0 = 0b00010,     MiscMem = 0b00011, OpImm = 0b00100,   AuiPc= 0b00101,        OpImm32 = 0b00110,    B480 = 0b00111,
     Store = 0b01000,    StoreFp = 0b01001, Custom1 = 0b01010,     Amo = 0b01011,      Op = 0b01100,       Lui = 0b01101,          Op32 = 0b01110,        B64 = 0b01111,
     MAdd = 0b10000,     MSub = 0b10001,     NMSub = 0b10010,        NMAdd = 0b10011,    OpFp = 0b10100,    Reserved0 = 0b10101,   Custom2 = 0b10110,     B481 = 0b10111,
