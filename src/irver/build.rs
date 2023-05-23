83037db2dc55e6504ac364cf9bd08da9d40d5d7f
John Jekel <JZJisawesome123@gmail.com>
2023-05-23 08:30:32 -0400

    Work on a Rust IRVE frontend

---
+++ src/irver/build.rs
@@ -0,0 +1,40 @@
+/* build.rs
+ * Copyright (C) 2023 John Jekel and Nick Chan
+ * See the LICENSE file at the root of the project for licensing info.
+ *
+ * Build script for irver
+ *
+ * Based on cbindgen and bindgen examples
+*/
+
+/* Imports */
+
+extern crate bindgen;
+
+/* Function Implementations */
+
+fn main() {
+    println!("cargo:rerun-if-changed=wrapper.h");
+    println!("cargo:rerun-if-changed=../../include/irve_disassemble.h");
+
+    let output_path = std::path::PathBuf::from(std::env::var("OUT_DIR").unwrap());
+
+    bindgen::Builder::default()
+        .header("wrapper.h")
+        .clang_arg("-xc++")//Needed since bidgen this the header is C
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("bindgen should sucessfully generate bindings")
+        .write_to_file(output_path.join("bindings.rs"))
+        .expect("bindgen should sucessfully write bindings to disk");
+}
+
+/* Tests */
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    //#[test]
+    //TODO
+}
---
+++ src/irver/wrapper.h
@@ -0,0 +1,16 @@
+/* wrapper.h
+ * Copyright (C) 2023 John Jekel and Nick Chan
+ * See the LICENSE file at the root of the project for licensing info.
+ *
+ * Wrapper header for bindgen to consume
+ *
+*/
+
+#ifndef WRAPPER_H
+#define WRAPPER_H
+
+/* Includes */
+
+#include "../../include/irve_public_api.h"
+
+#endif//WRAPPER_H
