ebadb35fdc7c907b7c3c57bdc0d3c7373611ed17
John Jekel <JZJisawesome123@gmail.com>
2023-06-04 19:32:13 -0400

    Begin working on implementing an SBI

---
+++ sbi/main.c
@@ -0,0 +1,41 @@
+/* main.c
+ * Copyright (C) 2023 John Jekel and Nick Chan
+ * See the LICENSE file at the root of the project for licensing info.
+ *
+ * Entry point into the IRVE SBI/Firmware
+ *
+*/
+
+/* Constants And Defines */
+
+//TODO
+
+/* Includes */
+
+#include <stdio.h>
+
+/* Types */
+
+//TODO
+
+/* Variables */
+
+//TODO
+
+/* Static Function Declarations */
+
+//TODO
+
+/* Function Implementations */
+
+int main() {
+#ifndef NDEBUG
+    puts("IRVE SBI and Firmware starting...");
+#endif
+
+    return 0;
+}
+
+/* Static Function Implementations */
+
+//TODO
