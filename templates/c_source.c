8a8d54a99fc196a348f16acb91519aec15e74989
John Jekel <JZJisawesome123@gmail.com>
2023-06-10 16:08:56 -0400

    Documentation improvements

---
+++ templates/c_source.c
@@ -0,0 +1,35 @@
+/* TODO.c
+ * Copyright (C) 2023 John Jekel and Nick Chan
+ * See the LICENSE file at the root of the project for licensing info.
+ *
+ * TODO description
+ *
+*/
+
+/* Constants And Defines */
+
+//TODO
+
+/* Includes */
+
+//TODO
+
+/* Types */
+
+//TODO
+
+/* Variables */
+
+//TODO
+
+/* Static Function Declarations */
+
+//TODO
+
+/* Function Implementations */
+
+//TODO
+
+/* Static Function Implementations */
+
+//TODO
