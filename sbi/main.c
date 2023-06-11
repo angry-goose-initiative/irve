124f3c2c55e84df0ccede367faae734793615eb0
John Jekel <JZJisawesome123@gmail.com>
2023-06-11 19:37:14 -0400

    Implement (stub) several exceptions that could occur

--- sbi/main.c
+++ sbi/main.c
@@ -113,7 +113,82 @@ void handle_other_exceptions(uint32_t registers[31], uint32_t mcause, uint32_t m
     dprintf("  mcause: 0x%lX\n", mcause);
     dprintf("  mepc:   0x%lX\n", mepc);
 
-    assert(false && "TODO implement");
+    switch (mcause) {
+        case 0://INSTRUCTION_ADDRESS_MISALIGNED_EXCEPTION
+            dputs("Instruction Address Misaligned Exception");
+            assert(false && "TODO implement misaligned accesses");
+            break;
+        case 1://INSTRUCTION_ACCESS_FAULT_EXCEPTION
+            dputs("Instruction Access Fault Exception");
+            dputs("  S-Mode code has just attempted to fetch an instruction from invalid memory or memory protected by PMP");
+            dputs("  This is likely a bug in S-Mode code!");
+            dputs("  I give up!");
+            irve_exit();
+            break;
+        case 2://ILLEGAL_INSTRUCTION_EXCEPTION
+            dputs("Illegal Instruction Exception");
+            assert(false && "TODO implement (emulate some instructions)");
+            break;
+        case 3://BREAKPOINT_EXCEPTION
+            dputs("Breakpoint Exception");
+            assert(false && "TODO implement (what to do for this?)");
+            break;
+        case 4://LOAD_ADDRESS_MISALIGNED_EXCEPTION
+            dputs("Load Address Misaligned Exception");
+            assert(false && "TODO implement misaligned accesses");
+            break;
+        case 5://LOAD_ACCESS_FAULT_EXCEPTION
+            dputs("Load Access Fault Exception");
+            dputs("  S-Mode code has just attempted to read from invalid memory or memory protected by PMP");
+            dputs("  This is likely a bug in S-Mode code!");
+            dputs("  I give up!");
+            irve_exit();
+            break;
+        case 6://STORE_OR_AMO_ADDRESS_MISALIGNED_EXCEPTION
+            dputs("Store or AMO Address Misaligned Exception");
+            assert(false && "TODO implement misaligned accesses (don't even bother with AMOs, just stores)");
+            break;
+        case 7://STORE_OR_AMO_ACCESS_FAULT_EXCEPTION
+            dputs("Store or AMO Access Fault Exception");
+            dputs("  S-Mode code has just attempted to store to or perform an AMO on invalid memory or memory protected by PMP");
+            dputs("  This is likely a bug in S-Mode code!");
+            dputs("  I give up!");
+            irve_exit();
+            break;
+        case 8://UMODE_ECALL_EXCEPTION
+            assert(false && "We should never get here; UMODE_ECALL_EXCEPTIONs should be delegated to S-Mode");
+            irve_exit();
+            break;
+        case 9://SMODE_ECALL_EXCEPTION
+            assert(false && "We should never get here; this should be handled by handle_smode_ecall()");
+            irve_exit();
+            break;
+        case 11://MMODE_ECALL_EXCEPTION
+            assert(false && "We should never get here; The IRVE SBI never does ECALL!");
+            irve_exit();
+            break;
+        case 12://INSTRUCTION_PAGE_FAULT_EXCEPTION
+            dputs("Instruction Page Fault Exception");
+            dputs("  S-Mode code has just attempted to fetch and instruction from invalid virtual memory");
+            dputs("  This is likely a bug in S-Mode code!");
+            dputs("  I give up!");
+            irve_exit();
+            break;
+        case 13://LOAD_PAGE_FAULT_EXCEPTION
+            dputs("Load Page Fault Exception");
+            dputs("  S-Mode code has just attempted read from invalid virtual memory");
+            dputs("  This is likely a bug in S-Mode code!");
+            dputs("  I give up!");
+            irve_exit();
+            break;
+        case 15://STORE_OR_AMO_PAGE_FAULT_EXCEPTION
+            dputs("Store or AMO Page Fault Exception");
+            dputs("  S-Mode code has just attempted to store to or perform an AMO on invalid virtual memory");
+            dputs("  This is likely a bug in S-Mode code!");
+            dputs("  I give up!");
+            irve_exit();
+            break;
+    }
 }
 
 /* Static Function Implementations */
