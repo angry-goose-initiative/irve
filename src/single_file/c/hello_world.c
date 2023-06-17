4efee457a3280f03e3eabd0a0156fc3f9ae2ef3e
John Jekel <JZJisawesome123@gmail.com>
2023-06-16 21:30:34 -0400

    Add the first test c program

---
+++ src/single_file/c/hello_world.c
@@ -0,0 +1,19 @@
+/* hello_world.c
+ * Copyright (C) 2023 John Jekel
+ * See the LICENSE file at the root of the project for licensing info.
+ *
+ * Prints "Hello World!" and exits.
+ *
+*/
+
+/* Includes */
+
+#include <stdio.h>
+
+/* Function Implementations */
+
+int main(void)
+{
+    puts(Hello World!);
+    return 0;
+}
