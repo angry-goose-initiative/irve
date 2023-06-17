3051d5df5a6ec6102101c1694c2ed250bb655310
John Jekel <JZJisawesome123@gmail.com>
2023-06-17 12:25:53 -0400

    Move more c programs to rvsw

--- rvsw/irve_rvsw_config.cmake
+++ rvsw/irve_rvsw_config.cmake
@@ -14,15 +14,15 @@ set(RVSW_COMMON_FLAGS_DEBUG "-O0 -g3")
 set(RVSW_COMMON_FLAGS_RELEASE "-O3 -flto=auto -fuse-linker-plugin")
 
 set(RVSW_MMODE_LD_SCRIPT "${PROJECT_SOURCE_DIR}/../irve_mmode.ld")
-set(RVSW_ENTRY_ADDR "0x00000000")#Your entry address here
-set(RVSW_MTVEC_IS_HARDWIRED "Vectored")#Uncomment if not hardwired, else "Direct" if direct, or "Vectored" if vectored
-set(RVSW_MTVEC_HARDWIRED_ADDR "0x00000004")#MTVEC hardwired address if applicable
-set(RVSW_MARCH "rv32ima_zicsr")#Your -march=[...] here
-set(RVSW_MABI "ilp32")#Your -mabi=[...] here
+set(RVSW_MTVEC_IS_HARDWIRED "VECTORED")
+set(RVSW_MARCH "rv32ima_zicsr")
+set(RVSW_MABI "ilp32")
 
 set(RVSW_EXIT_METHOD "CUSTOM_INSTRUCTION")
 set(RVSW_CUSTOM_EXIT_INSTR ".insn r CUSTOM_0, 0, 0, zero, zero, zero")
-#set(RVSW_CUSTOM_EXIT_FN "my_custom_exit_fn_symbol")
+
+set(RVSW_DATA_SEGMENT_ALREADY_LOADED "1")
+set(RVSW_BSS_SEGMENT_ALREADY_ZEROED "0")
 
 #Your own firmware must link against the SBI static library that is built
 set(RVSW_BUILD_SBI "ogsbi")#Uncomment if you want to build an SBI (and specify the name of the one you want to build)
