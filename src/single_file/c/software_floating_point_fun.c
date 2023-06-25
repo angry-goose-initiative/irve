bd04a5ca29cb703bd4caecfe1112f9db06e7ae8d
John Jekel <JZJisawesome123@gmail.com>
2023-06-25 07:39:47 -0400

    Do more in software_floating_point_fun

--- src/single_file/c/software_floating_point_fun.c
+++ src/single_file/c/software_floating_point_fun.c
@@ -7,6 +7,10 @@
  * Based on code from rv32esim
 */
 
+/* Constants And Defines */
+
+#define TOLERABLE_ERROR 0.0000000000001
+
 /* Includes */
 
 #include <stdio.h>
@@ -14,6 +18,7 @@
 #include <assert.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <math.h>
 
 /* Function Implementations */
 
@@ -22,20 +27,65 @@ int main() {
 
     volatile double a = 12.34;
     volatile double b = 5.678;
-
-    assert(a == 12.34);
-    assert(b == 5.678);
-    assert((a + b) == 18.018);
-    assert((a - b) == 6.662);
-    assert((a * b) == 70.06652);
-    assert((a / b) == 2.1733004579077142004);
+    volatile double zero;
+    volatile double pi = M_PI;
+    volatile double nan = NAN;
 
     printf("a is %f\n", a);
     printf("b is %f\n", b);
+    printf("0 is %f\n", zero);
+    printf("pi is %f\n", pi);
+    printf("nan is %f\n", nan);
     printf("a + b is %f\n", a + b);
     printf("a - b is %f\n", a - b);
     printf("a * b is %f\n", a * b);
     printf("a / b is %f\n", a / b);
+    printf("sin(zero) is %1.40f\n", sin(zero));
+    printf("cos(zero) is %1.40f\n", cos(zero));
+    printf("sin(pi) is %1.40f\n", sin(pi));
+    printf("cos(pi) is %1.40f\n", cos(pi));
+
+    assert(a == a);
+    assert(b == b);
+    assert(zero == zero);
+    assert(pi == pi);
+    assert(nan != nan);
+
+    assert(a == 12.34);
+    assert(b == 5.678);
+    assert(zero == 0.0);
+    assert(pi == M_PI);
+    assert(nan != NAN);
+    assert((a + b) == 18.018);
+    assert((a - b) == 6.662);
+    assert((a * b) == 70.06652);
+    assert((a / b) == 2.1733004579077142004);
+    assert(sin(0) == 0.0);
+    assert(cos(0) == 1.0);
+    assert(sin(pi) == 0.0000000000000001224646799147353207173764);//Not exactly 0 when I run it on my laptop
+    assert(cos(pi) == -1.0);
+
+    //TODO test other math functions
+
+    puts("Testing sqrt and cbrt");
+    for (volatile double i = 123.456; i < 567.890; i += 9.876) {
+        assert(sqrt(i) == pow(i, 0.5));
+        assert(abs(cbrt(i) - pow(i, 1.0 / 3.0)) < TOLERABLE_ERROR);
+    }
+
+    puts("Testing log and exp");
+    for (volatile double i = 123.456; i < 567.890; i += 9.876) {
+        volatile double temp = exp(i);
+        assert(log(temp) == i);
+        temp = log(i);
+        assert(abs(exp(temp) - i) < TOLERABLE_ERROR);
+        temp = exp2(i);
+        assert(abs(log2(temp) - i) < TOLERABLE_ERROR);
+        temp = log2(i);
+        assert(abs(exp2(temp) - i) < TOLERABLE_ERROR);
+    }
+
+    //TODO test others (ex cos and acos)
 
     return 0;
 }
