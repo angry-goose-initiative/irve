1af05d4b48bf0a09f358e82634bf1aa7eff616bb
John Jekel <JZJisawesome123@gmail.com>
2023-07-04 22:30:59 -0400

    Make UART progress

--- lib/uart.cpp
+++ lib/uart.cpp
@@ -13,7 +13,16 @@
  * Includes
  * --------------------------------------------------------------------------------------------- */
 
-//TODO
+#include <cassert>
+#include <cstdint>
+#include <string>
+
+#include "uart.h"
+
+#define INST_COUNT 0
+#include "logging.h"
+
+using namespace irve::internal;
 
 /* ------------------------------------------------------------------------------------------------
  * Constants/Defines
@@ -43,7 +52,94 @@
  * Function Implementations
  * --------------------------------------------------------------------------------------------- */
 
-//TODO
+uart::uart_t::uart_t() {
+    //TODO
+}
+
+uart::uart_t::~uart_t() {
+    if (this->m_output_line_buffer.size() > 0) {
+        irvelog_always_stdout(
+            0,
+            "\x1b[92mRV:\x1b[0m: \"\x1b[1m%s\x1b[0m\"",
+            this->m_output_line_buffer.c_str()
+        );
+    }
+}
+
+uint8_t uart::uart_t::read(uint8_t register_address) {
+    assert((register_address <= 0b111) && "Invalid UART register address!");
+    switch (register_address) {
+        //TODO implement
+        default: assert(false && "We should never get here!"); return 0;
+    }
+}
+
+void uart::uart_t::write(uint8_t register_address, uint8_t data) {
+    assert((register_address <= 0b111) && "Invalid UART register address!");
+    switch (register_address) {
+        case uart::address::THR: {//THR or DLL
+            if (this->dlab()) {//DLL
+                this->m_dll = data;
+            } else {//THR
+                //Note, because we "send" the character right away, the transmit fifo is always empty
+                if (((char)data) == '\n') {
+                    //End of line; print the contents of the line buffer to stdout
+                    irvelog_always_stdout(
+                        0,
+                        "\x1b[92mRV\x1b[0m: \"\x1b[1m%s\x1b[0m\\n\"",
+                        this->m_output_line_buffer.c_str()
+                    );
+                    this->m_output_line_buffer.clear();
+                } else {
+                    this->m_output_line_buffer.push_back((char)data);
+                }
+            }
+            break;
+        }
+        case uart::address::IER: {//IER or DLM
+            if (this->dlab()) {//DLM
+                this->m_dlm = data;
+            } else {//IER
+                assert(false && "TODO");//TODO
+            }
+            break;
+        }
+        case uart::address::FCR: {
+            assert(false && "TODO");//TODO
+            break;
+        }
+        case uart::address::LCR: {//LCR or PSD
+            if (this->dlab()) {//PSD
+                this->m_psd = data;
+            } else {//LCR
+                assert(false && "TODO");//TODO
+            }
+            break;
+        }
+        case uart::address::MCR: {
+            assert(false && "TODO");//TODO
+            break;
+        }
+        case uart::address::SPR: {
+            this->m_spr = data;
+            break;
+        }
+        case uart::address::LSR:
+        case uart::address::MSR:
+            //These registers aren't writable, but for simplicity, we'll ignore the write
+            //instead of adding infastructure to report an error to the user
+            break;
+        default: assert(false && "We should never get here!");
+    }
+}
+
+bool uart::uart_t::interrupt_pending() const {
+    return this->m_isr & 0b1;
+}
+
+bool uart::uart_t::dlab() const {
+    return this->m_lcr & (1 << 7);
+}
 
 /* ------------------------------------------------------------------------------------------------
  * Static Function Implementations
--- lib/uart.h
+++ lib/uart.h
@@ -16,28 +16,69 @@
  * Includes
  * --------------------------------------------------------------------------------------------- */
 
-//TODO
-
-/* ------------------------------------------------------------------------------------------------
- * Constants/Defines
- * --------------------------------------------------------------------------------------------- */
-
-//TODO
-
-/* ------------------------------------------------------------------------------------------------
- * Type/Class Declarations
- * --------------------------------------------------------------------------------------------- */
+#include <cstdint>
+#include <string>
 
 /**
  * @brief The namespace containing the IRVE's UART implementation
 */
 namespace irve::internal::uart {
+
+/* ------------------------------------------------------------------------------------------------
+ * Constants/Defines
+ * --------------------------------------------------------------------------------------------- */
+
+    namespace address {
+        const uint8_t RHR = 0b000;//Receive Holding Register
+        const uint8_t THR = 0b000;//Transmit Holding Register
+        const uint8_t IER = 0b001;//Interrupt Enable Register
+        const uint8_t ISR = 0b010;//Interrupt Status Register
+        const uint8_t FCR = 0b010;//FIFO Control Register
+        const uint8_t LCR = 0b011;//Line Control Register
+        const uint8_t MCR = 0b100;//Modem Control Register
+        const uint8_t LSR = 0b101;//Line Status Register
+        const uint8_t MSR = 0b110;//Modem Status Register
+        const uint8_t SPR = 0b111;//Scratch Pad Register
+
+        //When LCR.DLAP == 1
+        const uint8_t DLL = 0b000;//Divisor Latch LSB
+        const uint8_t DLM = 0b001;//Divisor Latch MSB
+        const uint8_t PSD = 0b010;//Prescaler Division
+    }
+
+/* ------------------------------------------------------------------------------------------------
+ * Type/Class Declarations
+ * --------------------------------------------------------------------------------------------- */
     //TODO
     /**
      * @brief The IRVE 16550 UART
     */
     class uart_t {
-    //TODO
+    public:
+        uart_t();
+        ~uart_t();
+        
+        uint8_t read(uint8_t register_address);//NOT const since it could pop from the FIFO
+        void write(uint8_t register_address, uint8_t data);
+
+        bool interrupt_pending() const;//More convenient than reading ISR and checking bits
+    private:
+        bool dlab() const;
+
+        //TODO other registers
+        uint8_t m_isr;//Interrupt Status Register
+        uint8_t m_lcr;//Line Control Register
+        //TODO other registers
+        uint8_t m_spr;//Scratch Pad Register
+        
+        //Note: We expose these registers, but we completely ignore their contents
+        //since the serial output is the terminal and there are no real "wires" to
+        //run at a particular baud rate
+        uint8_t m_dll;//Divisor Latch LSB
+        uint8_t m_dlm;//Divisor Latch MSB
+        uint8_t m_psd;//Prescaler Division
+
+        std::string m_output_line_buffer;
     };
 }
 
