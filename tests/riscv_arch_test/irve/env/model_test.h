71b2a2a46049be494ef85581577df0d46e07cfdb
John Jekel <JZJisawesome123@gmail.com>
2023-07-25 21:06:19 -0400

    More work on riscvof

--- tests/riscv_arch_test/irve/env/model_test.h
+++ tests/riscv_arch_test/irve/env/model_test.h
@@ -48,19 +48,6 @@
  * Constants/Defines
  * --------------------------------------------------------------------------------------------- */
 
-//TODO complete this
-
-#define RVMODEL_DATA_SECTION \
-        .pushsection .tohost,"aw",@progbits;                            \
-        .align 8; .global tohost; tohost: .dword 0;                     \
-        .align 8; .global fromhost; fromhost: .dword 0;                 \
-        .popsection;                                                    \
-        .align 8; .global begin_regstate; begin_regstate:               \
-        .word 128;                                                      \
-        .align 8; .global end_regstate; end_regstate:                   \
-        .word 4;
-
-//RV_COMPLIANCE_HALT
 #define RVMODEL_HALT                                              \
     li x1, 'H'; \
     sw x1, -1(zero); \
@@ -74,7 +61,7 @@
     sw x1, -1(zero); \
     .insn r CUSTOM_0, 0, 0, zero, zero, zero;
 
-#define RVMODEL_BOOT
+#define RVMODEL_BOOT \
     li x1, 'B'; \
     sw x1, -1(zero); \
     li x1, 'O'; \
@@ -86,41 +73,20 @@
     li x1, '\n'; \
     sw x1, -1(zero);
 
-//RV_COMPLIANCE_DATA_BEGIN
 #define RVMODEL_DATA_BEGIN                                              \
-  RVMODEL_DATA_SECTION                                                        \
-  .align 4;\
-  .global begin_signature; begin_signature:
+    .data; \
+    .align 4;\
+    .global irve_begin_signature; irve_begin_signature:
 
-//RV_COMPLIANCE_DATA_END
 #define RVMODEL_DATA_END                                                      \
   .align 4;\
-  .global end_signature; end_signature:  
+  .global irve_end_signature; irve_end_signature:
 
-//RVTEST_IO_INIT
 #define RVMODEL_IO_INIT
-//RVTEST_IO_WRITE_STR
 #define RVMODEL_IO_WRITE_STR(_R, _STR)
-//RVTEST_IO_CHECK
 #define RVMODEL_IO_CHECK()
-//RVTEST_IO_ASSERT_GPR_EQ
 #define RVMODEL_IO_ASSERT_GPR_EQ(_S, _R, _I)
-//RVTEST_IO_ASSERT_SFPR_EQ
 #define RVMODEL_IO_ASSERT_SFPR_EQ(_F, _R, _I)
-//RVTEST_IO_ASSERT_DFPR_EQ
 #define RVMODEL_IO_ASSERT_DFPR_EQ(_D, _R, _I)
-
-#define RVMODEL_SET_MSW_INT       \
- li t1, 1;                         \
- li t2, 0x2000000;                 \
- sw t1, 0(t2);
-
-#define RVMODEL_CLEAR_MSW_INT     \
- li t2, 0x2000000;                 \
- sw x0, 0(t2);
-
-#define RVMODEL_CLEAR_MTIMER_INT
-
-#define RVMODEL_CLEAR_MEXT_INT
 
 #endif//IRVE_MODEL_TEST_H
