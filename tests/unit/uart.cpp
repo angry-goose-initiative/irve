2f38b1d36a2f4b8d8b259fc34436abb771f5ee41
John Zacarias Jekel <john@jekel.ca>
2024-03-01 21:49:52 -0500

    Bring in more tests to test the UART with!

--- tests/unit/uart.cpp
+++ tests/unit/uart.cpp
@@ -38,7 +38,6 @@ using namespace irve::internal;
  * --------------------------------------------------------------------------------------------- */
 
 //TODO
-//TODO
 
 /* ------------------------------------------------------------------------------------------------
  * Static Function Declarations
@@ -53,8 +52,6 @@ using namespace irve::internal;
 int test_uart_Uart_sanity() {
     Uart uart;
 
-    //FIXME init the uart first
-
     uart.write(Uart::Address::THR, 'I');
     uart.write(Uart::Address::THR, 'R');
     uart.write(Uart::Address::THR, 'V');
@@ -67,19 +64,17 @@ int test_uart_Uart_sanity() {
 int test_uart_Uart_init() {
     Uart uart;
 
-    assert(uart.read(Uart::Address::RHR) == 0x00);
-    assert(uart.read(Uart::Address::THR) == 0x00);
     assert(uart.read(Uart::Address::IER) == 0x00);
     assert(uart.read(Uart::Address::ISR) == 0x01);
-    assert(uart.read(Uart::Address::FCR) == 0x00);
-    assert(uart.read(Uart::Address::LCR) == 0x00);
+    //assert(uart.read(Uart::Address::FCR) == 0x00);//Sadly this register isn't readable
+    assert(uart.read(Uart::Address::LCR) == 0x03);//Instead of 0x00 since we only support 8-bit characters
     assert(uart.read(Uart::Address::MCR) == 0x00);
     assert(uart.read(Uart::Address::LSR) == 0x60);
     //Not testing MSR since there is some reset weirdness
     assert(uart.read(Uart::Address::SPR) == 0x00);
 
+    uart.write(Uart::Address::LCR, (1 << 7) | 0x03);//Set LCR.DLAB
     //No need to test DLL or DLM since they are undefined at reset
-    uart.write(Uart::Address::LCR, (1 << 7));//Set LCR.DLAB
     assert(uart.read(Uart::Address::PSD) == 0x00);
 
     return 0;
